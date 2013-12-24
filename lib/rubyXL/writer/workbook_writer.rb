e166c5ae27cf7be9bd8602631847cbb4ee1772bd
weshatheleopard <github@wesha.name>
2013-12-24 16:44:44 -0600

    Moving away from Nokogiri::XML::Builder

--- lib/rubyXL/writer/workbook_writer.rb
+++ lib/rubyXL/writer/workbook_writer.rb
@@ -2,73 +2,70 @@ require 'rubygems'
 require 'nokogiri'
 
 module RubyXL
-module Writer
-  class WorkbookWriter < GenericWriter
+  module Writer
+    class WorkbookWriter < GenericWriter
 
-    def filepath
-      File.join('xl', 'workbook.xml')
-    end
-
-    def write()
-      contents = build_xml do |xml|
-        xml.workbook('xmlns'=>"http://schemas.openxmlformats.org/spreadsheetml/2006/main",
-        'xmlns:r'=>"http://schemas.openxmlformats.org/officeDocument/2006/relationships") {
-          #attributes out of order here
-          xml.fileVersion('appName'=>'xl', 'lastEdited'=>'4','lowestEdited'=>'4','rupBuild'=>'4505')
-          #TODO following line - date 1904? check if mac only
-          if @workbook.date1904.nil? || @workbook.date1904.to_s == ''
-            xml.workbookPr('showInkAnnotation'=>'0', 'autoCompressPictures'=>'0')
-          else
-            xml.workbookPr('date1904'=>@workbook.date1904.to_s, 'showInkAnnotation'=>'0', 'autoCompressPictures'=>'0')
-          end
-          xml.bookViews {
-            #attributes out of order here
-            xml.workbookView('xWindow'=>'-20', 'yWindow'=>'-20',
-              'windowWidth'=>'21600','windowHeight'=>'13340','tabRatio'=>'500')
-          }
-
-          index = 0
-          xml.sheets do
-            @workbook.worksheets.each_with_index { |sheet, i|
-              index = i + 1
-              xml.sheet('name'=> sheet.sheet_name,
-                        'sheetId' => sheet.sheet_id || index,
-                        'r:id'=> "rId#{index}")
-            }
-          end
-
-          unless @workbook.external_links.empty?
-            xml.externalReferences do
-              # This doesn't quite make a lot of sense -- why we are starting with index and not 0?
-              # Need to check once I get the file with external links...
-              index.upto(@workbook.external_links.size - 1) { |id|
-                xml.externalReference('r:id' => "rId#{id + index}")
-              }
-            end
-          end
-
-          # nokogiri builder creates CDATA tag around content,
-          # using .text creates "html safe" &lt; and &gt; in place of < and >
-          # xml to hash method does not seem to function well for this particular piece of xml
-          xml.cdata @workbook.defined_names.to_s
-
-          #TODO see if this changes with formulas
-          #attributes out of order here
-          xml.calcPr('calcId'=>'130407', 'concurrentCalc'=>'0')
-          xml.extLst {
-            xml.ext('xmlns:mx'=>"http://schemas.microsoft.com/office/mac/excel/2008/main",
-            'uri'=>"http://schemas.microsoft.com/office/mac/excel/2008/main") {
-              xml['mx'].ArchID('Flags'=>'2')
-            }
-          }
-        }
+      def filepath
+        File.join('xl', 'workbook.xml')
       end
 
-      # See comment above about CDATA. This needs to be implemented properly.
-      contents.gsub(/<!\[CDATA\[(.*)\]\]>/, '\1')
+      def write()
+        new_xml = render_xml do |xml|
+          xml << (xml.create_element('workbook',
+                  :xmlns => 'http://schemas.openxmlformats.org/spreadsheetml/2006/main',
+                  'xmlns:r' => 'http://schemas.openxmlformats.org/officeDocument/2006/relationships') { |root|
+            root << xml.create_element('fileVersion', { :appName => 'xl', :lastEdited => 4,
+                                                        :lowestEdited => 4, :rupBuild => 4505 })
+
+            params = { :showInkAnnotation => 0, :autoCompressPictures => 0 }
+
+            #TODO following line - date 1904? check if mac only
+            params[:date1904] = 1 if @workbook.date1904
+            root << xml.create_element('workbookPr', params)
+
+            root << (xml.create_element('bookViews') { |views|
+              views << xml.create_element('workbookView', { :xWindow => -20, :yWindow => -20,
+                                            :windowWidth => 21600, :windowHeight => 13340, :tabRatio => 500 })
+            })
+
+            index = 0
+            root << (xml.create_element('sheets') { |sheet_xml|
+              @workbook.worksheets.each_with_index { |sheet, i|
+                index = i + 1
+                sheet_xml << xml.create_element('sheet', { :name => sheet.sheet_name,
+                                                           :sheetId => sheet.sheet_id || index, 
+                                                           'r:id'=> "rId#{index}" })
+              }
+            })
+
+            unless @workbook.external_links.empty?
+              root << (xml.create_element('externalReferences') { |refs|
+                # This doesn't quite make a lot of sense -- why we are starting with index and not 0?
+                # Need to check once I get the file with external links...
+                index.upto(@workbook.external_links.size - 1) { |id|
+                  refs << xml..create_element('externalReference', { 'r:id' => "rId#{id + index}" })
+                }
+              })
+            end
+
+#            @workbook.defined_names.each { |name| xml << name.write_xml(xml) }
+  
+            #TODO see if this changes with formulas
+            #attributes out of order here
+            root << xml.create_element('calcPr', { :calcId => 130407, :concurrentCalc => 0 } )
+
+            root << (xml.create_element('extLst') { |list| 
+              list << (xml.create_element('ext',  {
+                         'xmlns:mx' => 'http://schemas.microsoft.com/office/mac/excel/2008/main',
+                         :uri => 'http://schemas.microsoft.com/office/mac/excel/2008/main'}) { |ext|
+                ext << xml.create_element('mx:ArchID', { :Flags => 2 })
+
+              })
+            })
+          })
+        end
+      end
 
     end
-
   end
-end
 end
