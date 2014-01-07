fb333a5a27c66cd5a0e805f501ffcf57fb58005a
weshatheleopard <github@wesha.name>
2014-01-07 15:54:19 -0600

    Retiring Nokogiri::Builder

--- lib/rubyXL/writer/workbook_rels_writer.rb
+++ lib/rubyXL/writer/workbook_rels_writer.rb
@@ -12,53 +12,33 @@ module Writer
 
     #all attributes out of order
     def write()
+      rels = []
+
+      @workbook.worksheets.each_index { |i|
+        rels << [ "worksheets/sheet#{i + 1}.xml", 'worksheet' ]
+      }
+
+      @workbook.external_links.each_key { |k| 
+        rels << [ "externalLinks/#{k}", 'externalLink' ]
+      }
+
+      rels << [ 'theme/theme1.xml', 'theme' ]
+      rels << [ 'styles.xml', 'styles' ]
+      rels << [ 'sharedStrings.xml', 'sharedStrings' ] unless @workbook.shared_strings.empty?
+
       render_xml do |xml|
-
-        i = 1
-
         xml << (xml.create_element('Relationships',
                 :xmlns => 'http://schemas.openxmlformats.org/package/2006/relationships') { |root|
 
-          @workbook.worksheets.each { |sheet|
+          rels.each_with_index { |rel, i|
             root << xml.create_element('Relationship',
-              { :Id => "rId#{i}",
-                :Type => 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet',
-                :Target => "worksheets/sheet#{i}.xml" })
-            i += 1
+              { :Id => "rId#{i + 1}",
+                :Type => "http://schemas.openxmlformats.org/officeDocument/2006/relationships/#{rel.last}",
+                :Target => rel.first })
           }
-
-          unless @workbook.external_links.nil?
-            1.upto(@workbook.external_links.size - 1) { |link|
-
-              root << xml.create_element('Relationship',
-                { :Id => "rId#{i}",
-                  :Type => 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/externalLink',
-                  :Target => "externalLinks/externalLink#{link}.xml" })
-              i += 1
-            }
-          end
-
-          root << xml.create_element('Relationship',
-            { :Id => "rId#{i}",
-              :Type => 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme',
-              :Target => 'theme/theme1.xml' })
-          i += 1
-
-          root << xml.create_element('Relationship',
-            { :Id => "rId#{i}",
-              :Type => 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles',
-              :Target => 'styles.xml' })
-          i += 1
-
-          unless @workbook.shared_strings.empty?
-            root << xml.create_element('Relationship',
-              { :Id => "rId#{i}",
-                :Type => 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings',
-                :Target => 'sharedStrings.xml' })
-            i += 1
-          end
         })
       end
+
     end
 
   end
