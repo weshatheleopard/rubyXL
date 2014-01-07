2bc2401718f6bde6d56860712b06b36de9cddf4b
weshatheleopard <github@wesha.name>
2014-01-07 15:55:15 -0600

    Whitespace fix

--- lib/rubyXL/writer/workbook_rels_writer.rb
+++ lib/rubyXL/writer/workbook_rels_writer.rb
@@ -2,45 +2,45 @@ require 'rubygems'
 require 'nokogiri'
 
 module RubyXL
-module Writer
+  module Writer
 
-  class WorkbookRelsWriter < GenericWriter
+    class WorkbookRelsWriter < GenericWriter
 
-    def filepath
-      File.join('xl', '_rels', 'workbook.xml.rels')
-    end
+      def filepath
+        File.join('xl', '_rels', 'workbook.xml.rels')
+      end
 
-    #all attributes out of order
-    def write()
-      rels = []
+      #all attributes out of order
+      def write()
+        rels = []
 
-      @workbook.worksheets.each_index { |i|
-        rels << [ "worksheets/sheet#{i + 1}.xml", 'worksheet' ]
-      }
+        @workbook.worksheets.each_index { |i|
+          rels << [ "worksheets/sheet#{i + 1}.xml", 'worksheet' ]
+        }
 
-      @workbook.external_links.each_key { |k| 
-        rels << [ "externalLinks/#{k}", 'externalLink' ]
-      }
+        @workbook.external_links.each_key { |k| 
+          rels << [ "externalLinks/#{k}", 'externalLink' ]
+        }
 
-      rels << [ 'theme/theme1.xml', 'theme' ]
-      rels << [ 'styles.xml', 'styles' ]
-      rels << [ 'sharedStrings.xml', 'sharedStrings' ] unless @workbook.shared_strings.empty?
+        rels << [ 'theme/theme1.xml', 'theme' ]
+        rels << [ 'styles.xml', 'styles' ]
+        rels << [ 'sharedStrings.xml', 'sharedStrings' ] unless @workbook.shared_strings.empty?
 
-      render_xml do |xml|
-        xml << (xml.create_element('Relationships',
-                :xmlns => 'http://schemas.openxmlformats.org/package/2006/relationships') { |root|
+        render_xml do |xml|
+          xml << (xml.create_element('Relationships',
+                  :xmlns => 'http://schemas.openxmlformats.org/package/2006/relationships') { |root|
 
-          rels.each_with_index { |rel, i|
-            root << xml.create_element('Relationship',
-              { :Id => "rId#{i + 1}",
-                :Type => "http://schemas.openxmlformats.org/officeDocument/2006/relationships/#{rel.last}",
-                :Target => rel.first })
-          }
-        })
+            rels.each_with_index { |rel, i|
+              root << xml.create_element('Relationship',
+                { :Id => "rId#{i + 1}",
+                  :Type => "http://schemas.openxmlformats.org/officeDocument/2006/relationships/#{rel.last}",
+                  :Target => rel.first })
+            }
+          })
+        end
+
       end
 
     end
-
   end
-end
 end
