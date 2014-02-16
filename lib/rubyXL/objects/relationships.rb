db25db74e9a77dc74b2de82b2621092d99c83140
weshatheleopard <github@wesha.name>
2014-02-16 02:33:33 -0600

    Issue #87: Moving Writer functionality to OOXMLTopLevelObject

--- lib/rubyXL/objects/relationships.rb
+++ lib/rubyXL/objects/relationships.rb
@@ -13,51 +13,83 @@ module RubyXL
     define_element_name 'Relationship'
   end
 
-  # http://www.schemacentral.com/sc/ooxml/e-ssml_calcChain.html
-  class WorkbookRelationships < OOXMLTopLevelObject
+  class OOXMLRelationshipsFile < OOXMLTopLevelObject
     define_child_node(RubyXL::Relationship, :collection => true, :accessor => :relationships)
     define_element_name 'Relationships'
     set_namespaces('xmlns' => 'http://schemas.openxmlformats.org/package/2006/relationships')
 
-    attr_accessor :workbook
-
-    def create_relationship(target, type)
+    def document_relationship(target, type)
       RubyXL::Relationship.new(:id => "rId#{relationships.size + 1}", 
                                :type => "http://schemas.openxmlformats.org/officeDocument/2006/relationships/#{type}",
                                :target => target)
     end
+    protected :document_relationship
+
+    def metadata_relationship(target, type)
+      RubyXL::Relationship.new(:id => "rId#{relationships.size + 1}", 
+                               :type => "http://schemas.openxmlformats.org/package/2006/relationships/metadata/#{type}",
+                               :target => target)
+    end
+    protected :metadata_relationship
+
+    def find_by_rid(r_id)
+      relationships.find { |r| r.id == r_id }
+    end
+  end
+
+
+  class WorkbookRelationships < OOXMLRelationshipsFile
+
+    attr_accessor :workbook
 
     def before_write_xml
       self.relationships = []
 
       @workbook.worksheets.each_with_index { |sheet, i|
-        relationships << create_relationship(sheet.filepath.gsub(/^xl\//, ''), sheet.rel_type)
+        relationships << document_relationship(sheet.filepath.gsub(/^xl\//, ''), sheet.rel_type)
       }
 
       @workbook.external_links.each_key { |k| 
-        relationships << create_relationship("externalLinks/#{k}", 'externalLink')
+        relationships << document_relationship("externalLinks/#{k}", 'externalLink')
       }
 
-      relationships << create_relationship('theme/theme1.xml', 'theme')
-      relationships << create_relationship('styles.xml', 'styles')
+      relationships << document_relationship('theme/theme1.xml', 'theme')
+      relationships << document_relationship('styles.xml', 'styles')
 
       if @workbook.shared_strings_container && !@workbook.shared_strings_container.strings.empty? then
-        relationships << create_relationship('sharedStrings.xml', 'sharedStrings') 
+        relationships << document_relationship('sharedStrings.xml', 'sharedStrings') 
       end
 
       if @workbook.calculation_chain && !@workbook.calculation_chain.cells.empty? then
-        relationships << create_relationship('calcChain.xml', 'calcChain') 
+        relationships << document_relationship('calcChain.xml', 'calcChain') 
       end
 
       true
     end
 
-    def find_by_rid(r_id)
-      relationships.find { |r| r.id == r_id }
-    end
-
     def self.filepath
       File.join('xl', '_rels', 'workbook.xml.rels')
     end
+
   end
+
+  class RootRelationships < OOXMLRelationshipsFile
+
+    attr_accessor :workbook
+
+    def before_write_xml
+      self.relationships = []
+
+      relationships << document_relationship('xl/workbook.xml', 'officeDocument')
+      relationships << metadata_relationship('docProps/core.xml', 'core-properties')
+      relationships << document_relationship('docProps/app.xml', 'extended-properties')
+
+      true
+    end
+
+    def self.filepath
+      File.join('_rels', '.rels')
+    end
+  end
+
 end
