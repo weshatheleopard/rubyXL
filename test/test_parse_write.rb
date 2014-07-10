bbf11a152a7907a38d8bf01781a2f3f8d60acc4d
weshatheleopard <github@wesha.name>
2014-07-10 21:48:53 +0000

    Profiling attempts

--- test/test_parse_write.rb
+++ test/test_parse_write.rb
@@ -1,15 +1,19 @@
 require 'rubyXL'
 require 'benchmark'
+require 'stackprof'
 
 spreadsheets = Dir.glob(File.join("test", "input", "*.xls?")).sort!
 
 spreadsheets.each { |input|
   puts "<<<--- Parsing #{input}..."
   doc = nil
-  tm = Benchmark.realtime { doc = RubyXL::Parser.parse(input) }
-  puts "Elapsed: #{tm} sec"
-  output = File.join("test", "output", File.basename(input))
-  puts  "--->>> Writing #{output}..."
-  tm = Benchmark.realtime { doc.write(output) }
-  puts "Elapsed: #{tm} sec"
+#  tm = Benchmark.realtime { doc = RubyXL::Parser.parse(input) }
+#  puts "Elapsed: #{tm} sec"
+  StackProf.run(mode: :cpu, interval: 100, out: "stackprof-cpu-read-#{File.basename(input)}.dump") { doc = RubyXL::Parser.parse(input) }
+
+#  output = File.join("test", "output", File.basename(input))
+#  puts  "--->>> Writing #{output}..."
+#  StackProf.run(mode: :cpu, out: "stackprof-cpu-write-#{File.basename(input)}.dump") { doc.write(output) }
+#  tm = Benchmark.realtime 
+#  puts "Elapsed: #{tm} sec"
 }
