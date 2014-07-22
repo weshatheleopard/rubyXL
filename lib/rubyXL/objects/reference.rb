82e02306574e22a8f2bb4d03611390eab528fc70
weshatheleopard <github@wesha.name>
2014-07-22 23:32:31 +0000

    Further implementation of Reference

--- lib/rubyXL/objects/reference.rb
+++ lib/rubyXL/objects/reference.rb
@@ -3,28 +3,39 @@ module RubyXL
     ROW_MAX = 1024*1024
     COL_MAX = 16393
 
-    attr_reader :first_row, :last_row, :first_col, :last_col
+    attr_reader :first_row, :last_row, :first_col, :last_col, :first_sheet, :last_sheet
+    attr_accessor :base
 
     # RubyXL::Reference.new(row, col)
     # RubyXL::Reference.new(row_from, row_to, col_from, col_to)
     # RubyXL::Reference.new(reference_string)
     def initialize(*params)
-      row_from = row_to = col_from = col_to = nil
+      @first_sheet = @last_sheet = row_from = row_to = col_from = col_to = nil
 
       case params.size
       when 4 then row_from, row_to, col_from, col_to = params
       when 2 then row_from, col_from = params
       when 1 then
         raise ArgumentError.new("invalid value for #{self.class}: #{params[0].inspect}") unless params[0].is_a?(String)
-        from, to = params[0].split(':')
-        row_from, col_from = self.class.ref2ind(from)
-        row_to, col_to = self.class.ref2ind(to) unless to.nil?
+        str = params[0]
+
+        str =~ /\A(?:(?:(?:'([^':]+)(?:\:([^':]+))?)'|(?:(\w+)(?:\:(\w+))?))\!)?(\$?[A-Z]+\$?\d+)(?:\:(\$?[A-Z]+\$?\d+))?\Z/
+
+        @first_sheet = $1 || $3
+        @last_sheet  = $2 || $4 || @first_sheet
+        row_from, col_from, row_from_abs, col_from_abs = self.class.ref2ind($5)
+        row_to, col_to, row_to_abs, col_to_abs = self.class.ref2ind($6) unless $6.nil?
       end
 
       @first_row = row_from || 0
       @last_row  = row_to || row_from || ROW_MAX
       @first_col = col_from || 0
       @last_col  = col_to || col_from || COL_MAX
+      @first_row_abs = row_from_abs
+      @first_col_abs = col_from_abs
+      @last_row_abs  = row_to_abs || row_from_abs
+      @first_col_abs = col_to_abs || col_from_abs
+      @base = nil
     end
 
     def single_cell?
@@ -45,7 +56,7 @@ module RubyXL
 
     def to_s
       if single_cell? then
-        self.class.ind2ref(@first_row, @first_col)
+        self.class.ind2ref(@first_row, @first_col, @first_row_abs, @first_col_abs)
       else
         self.class.ind2ref(@first_row, @first_col) + ':' + self.class.ind2ref(@last_row, @last_col)
       end
@@ -61,7 +72,7 @@ module RubyXL
 
     # Converts +row+ and +col+ zero-based indices to Excel-style cell reference
     # (0) A...Z, AA...AZ, BA... ...ZZ, AAA... ...AZZ, BAA... ...XFD (16383)
-    def self.ind2ref(row = 0, col = 0)
+    def self.ind2ref(row = 0, col = 0, row_abs = false, col_abs = false)
       str = ''
 
       loop do
@@ -71,16 +82,19 @@ module RubyXL
         break if col < 0
       end
 
+      str = ('$' + str) if col_abs
+      str += '$' if row_abs
+
       str += (row + 1).to_s
     end
 
     # Converts Excel-style cell reference to +row+ and +col+ zero-based indices.
     def self.ref2ind(str)
-      return [ -1, -1 ] unless str =~ /\A([A-Z]+)(\d+)\Z/
+      return [ -1, -1 ] unless str =~ /\A(\$)?([A-Z]+)(\$)?(\d+)\Z/
 
       col = 0
-      $1.each_byte { |chr| col = col * 26 + (chr - 64) }
-      [ $2.to_i - 1, col - 1 ]
+      $2.each_byte { |chr| col = col * 26 + (chr - 64) }
+      [ $4.to_i - 1, col - 1, !$3.nil?, !$1.nil? ]
     end
 
   end
--- spec/lib/reference_spec.rb
+++ spec/lib/reference_spec.rb
@@ -13,17 +13,91 @@ describe RubyXL::Reference do
       expect(RubyXL::Reference.ind2ref(0, 77)).to eq('BZ1')
       expect(RubyXL::Reference.ind2ref(0, 78)).to eq('CA1')
       expect(RubyXL::Reference.ind2ref(0, 16383)).to eq('XFD1')
+
+      expect(RubyXL::Reference.ind2ref(0, 1, false, false)).to eq('B1')
+      expect(RubyXL::Reference.ind2ref(0, 1, true,  false)).to eq('B$1')
+      expect(RubyXL::Reference.ind2ref(0, 1, false, true )).to eq('$B1')
+      expect(RubyXL::Reference.ind2ref(0, 1, true,  true )).to eq('$B$1')
     end
 
     it 'should correctly convert back and forth between "Excel Style" and index style cell references' do
       0.upto(16383) do |n|
-        expect(RubyXL::Reference.ref2ind(RubyXL::Reference.ind2ref(n, 16383 - n))).to eq([ n, 16383 - n ])
+        expect(RubyXL::Reference.ref2ind(RubyXL::Reference.ind2ref(n, 16383 - n, false, false))).to eq([ n, 16383 - n, false, false ])
+        expect(RubyXL::Reference.ref2ind(RubyXL::Reference.ind2ref(n, 16383 - n, true,  false))).to eq([ n, 16383 - n, true,  false ])
+        expect(RubyXL::Reference.ref2ind(RubyXL::Reference.ind2ref(n, 16383 - n, false, true ))).to eq([ n, 16383 - n, false, true  ])
+        expect(RubyXL::Reference.ref2ind(RubyXL::Reference.ind2ref(n, 16383 - n, true,  true ))).to eq([ n, 16383 - n, true,  true  ])
       end
     end
 
     it 'should return [-1, -1] if the Excel index is not well-formed' do
       expect(RubyXL::Reference.ref2ind('A1B')).to eq([-1, -1])
     end
+
+    it 'should properly parse cases from the manual' do
+      # The cell in column A and row 10
+      ref = RubyXL::Reference.new('A10')
+      expect(ref.single_cell?).to eq(true)
+      expect(ref.first_row).to eq(9)
+      expect(ref.first_col).to eq(0)
+
+      # The range of cells in column A and rows 10 through 20
+      ref = RubyXL::Reference.new('A10:A20')
+      expect(ref.single_cell?).to eq(false)
+      expect(ref.first_row).to eq(9)
+      expect(ref.first_col).to eq(0)
+      expect(ref.last_row).to eq(19)
+      expect(ref.last_col).to eq(0)
+
+      # The range of cells in row 15 and columns B through E
+      ref = RubyXL::Reference.new('B15:E15')
+      expect(ref.single_cell?).to eq(false)
+      expect(ref.first_row).to eq(14)
+      expect(ref.first_col).to eq(1)
+      expect(ref.last_row).to eq(14)
+      expect(ref.last_col).to eq(4)
+
+      # All cells in row 5
+      ref = RubyXL::Reference.new('5:5')
+      expect(ref.single_cell?).to eq(false)
+      expect(ref.first_row).to eq(4)
+      expect(ref.first_col).to be_nil
+      expect(ref.last_row).to eq(4)
+      expect(ref.last_col).to be_nil
+
+      # All cells in rows 5 through 10
+      ref = RubyXL::Reference.new('5:10')
+      expect(ref.single_cell?).to eq(false)
+      expect(ref.first_row).to eq(4)
+      expect(ref.first_col).to be_nil
+      expect(ref.last_row).to eq(9)
+      expect(ref.last_col).to be_nil
+
+      # All cells in column H
+      ref = RubyXL::Reference.new('H:H')
+      expect(ref.single_cell?).to eq(false)
+      expect(ref.first_row).to be_nil
+      expect(ref.first_col).to eq(7)
+      expect(ref.last_row).to be_nil
+      expect(ref.last_col).to eq(7)
+
+      # All cells in columns H through J
+      ref = RubyXL::Reference.new('H:J')
+      expect(ref.single_cell?).to eq(false)
+      expect(ref.first_row).to be_nil
+      expect(ref.first_col).to eq(7)
+      expect(ref.last_row).to be_nil
+      expect(ref.last_col).to eq(8)
+
+      # The range of cells in row 15 and columns B through E
+      ref = RubyXL::Reference.new('A10:E20')
+      expect(ref.single_cell?).to eq(false)
+      expect(ref.first_row).to eq(9)
+      expect(ref.first_col).to eq(1)
+      expect(ref.last_row).to eq(19)
+      expect(ref.last_col).to eq(4)
+
+    end
+
   end
 
 end
