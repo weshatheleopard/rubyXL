9b3d08ec6ebd331c1aff740844d7fb7ee2abb242
Jeremy Thibeaux <jthibeaux@gmail.com>
2015-01-30 16:40:11 -0800

    expose parse_buffer from RubyXL object

--- lib/rubyXL/parser.rb
+++ lib/rubyXL/parser.rb
@@ -4,5 +4,10 @@ module RubyXL
       root = RubyXL::WorkbookRoot.parse_file(xl_file_path)
       root.workbook
     end
+
+    def self.parse_buffer(buffer)
+      root = RubyXL::WorkbookRoot.parse_buffer(buffer)
+      root.workbook
+    end
   end
 end
