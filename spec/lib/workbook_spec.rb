ca73ea019abdbc7c7bfd805b19c5cf65a60ac32e
weshatheleopard <github@wesha.name>
2017-12-12 21:32:53 +0000

    Issue #283 - suppress warning

--- spec/lib/workbook_spec.rb
+++ spec/lib/workbook_spec.rb
@@ -136,7 +136,7 @@ describe RubyXL::Workbook do
     it "It should not be confused by missing sheet_id" do
       workbook = RubyXL::Workbook.new
       workbook[0].sheet_id = 1
-      sheet = workbook.add_worksheet('Sheet2')
+      workbook.add_worksheet('Sheet2')
       workbook.stream
     end
 
